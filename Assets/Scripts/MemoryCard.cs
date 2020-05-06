4f32e85da9f8b080f4d1dde64b7b2f946b5392a2
Reid Harry <reidoharry50@gmail.com>
2020-05-06 12:10:28 -0500

    Setup and testing card clicking

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField] private GameObject cardBack;
+    [SerializeField] private Sprite image;
+
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+
+    private void OnMouseDown()
+    {
+        if (cardBack.activeSelf)
+        {
+            cardBack.SetActive(false);
+        }
+    }
+}
